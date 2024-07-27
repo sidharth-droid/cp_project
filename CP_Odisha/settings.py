2b2c0b41ea96b453bc76c5920dfa689cbaebf614
sidharth-droid <sidharth2432004@gmail.com>
2024-07-28 00:16:37 +0530

    deleted docker

--- CP_Odisha/settings.py
+++ CP_Odisha/settings.py
@@ -122,7 +122,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ComplainApp', 'static'),
 ]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
